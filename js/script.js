6e5f3d544e9bf2d0ea76f8300ba82e73cbb43824
SungmiKim <62002100+SungmiKim@users.noreply.github.com>
2020-07-06 19:21:38 +0900

    2020-07-06
    2020-07-06

--- js/script.js
+++ js/script.js
@@ -15,3 +15,35 @@ function cancel() {
 function ready() {
   alert("준비중 입니다 ^-^");
 }
+
+function titleChangeSecond() {
+  var titleFirst = document.getElementById("title-first");
+  var titleSecond = document.getElementById("title-second");
+  var firstContent = document.getElementById("find__first-content");
+  var secondContent = document.getElementById("find__second-content");
+
+  titleFirst.style.fontWeight = 500;
+  titleFirst.style.borderBottom = "none";
+  titleSecond.style.fontWeight = 600;
+  titleFirst.textContent = "코로나";
+  titleSecond.textContent = "#뉴스";
+  titleSecond.style.borderBottom = "2px solid black";
+  firstContent.style.visibility = "hidden";
+  secondContent.style.visibility = "visible";
+}
+
+function titleChangeFirst() {
+  var titleFirst = document.getElementById("title-first");
+  var titleSecond = document.getElementById("title-second");
+  var firstContent = document.getElementById("find__first-content");
+  var secondContent = document.getElementById("find__second-content");
+
+  titleSecond.style.fontWeight = 500;
+  titleSecond.style.borderBottom = "none";
+  titleSecond.textContent = "뉴스";
+  titleFirst.style.fontWeight = 600;
+  titleFirst.textContent = "#코로나";
+  titleFirst.style.borderBottom = "2px solid black";
+  firstContent.style.visibility = "visible";
+  secondContent.style.visibility = "hidden";
+}
