3ad96fe7a02ad0842531451824945cb50ca8e239
SungmiKim <62002100+SungmiKim@users.noreply.github.com>
2020-07-07 15:54:45 +0900

    2020-07-07
    2020-07-07

--- js/script.js
+++ js/script.js
@@ -1,14 +1,15 @@
-function search() {
-  const searchContent = document.getElementById("friends__search-content");
-  const friends = document.getElementById("friends");
+const searchContent = document.getElementById("friends__search-content");
+const friends = document.getElementById("friends");
+const titleFirst = document.getElementById("title-first");
+const titleSecond = document.getElementById("title-second");
+const firstContent = document.getElementById("find__first-content");
+const secondContent = document.getElementById("find__second-content");
 
+function search() {
   friends.style.opacity = 0.5;
   searchContent.style.opacity = 1;
 }
 function cancel() {
-  const searchContent = document.getElementById("friends__search-content");
-  const friends = document.getElementById("friends");
-
   friends.style.opacity = 1;
   searchContent.style.opacity = 0;
 }
@@ -17,15 +18,10 @@ function ready() {
 }
 
 function titleChangeSecond() {
-  const titleFirst = document.getElementById("title-first");
-  const titleSecond = document.getElementById("title-second");
-  const firstContent = document.getElementById("find__first-content");
-  const secondContent = document.getElementById("find__second-content");
-
   titleFirst.style.fontWeight = 500;
   titleFirst.style.borderBottom = "none";
-  titleSecond.style.fontWeight = 600;
   titleFirst.textContent = "코로나";
+  titleSecond.style.fontWeight = 600;
   titleSecond.textContent = "#뉴스";
   titleSecond.style.borderBottom = "2px solid black";
   firstContent.style.visibility = "hidden";
@@ -33,17 +29,12 @@ function titleChangeSecond() {
 }
 
 function titleChangeFirst() {
-  const titleFirst = document.getElementById("title-first");
-  const titleSecond = document.getElementById("title-second");
-  const firstContent = document.getElementById("find__first-content");
-  const secondContent = document.getElementById("find__second-content");
-
-  titleSecond.style.fontWeight = 500;
-  titleSecond.style.borderBottom = "none";
-  titleSecond.textContent = "뉴스";
   titleFirst.style.fontWeight = 600;
   titleFirst.textContent = "#코로나";
   titleFirst.style.borderBottom = "2px solid black";
+  titleSecond.style.fontWeight = 500;
+  titleSecond.style.borderBottom = "none";
+  titleSecond.textContent = "뉴스";
   firstContent.style.visibility = "visible";
   secondContent.style.visibility = "hidden";
 }
